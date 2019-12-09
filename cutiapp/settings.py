13bc022463bda444b3e53de3d635eca68e5bfa30
avifmuhamadtaufiq <avifmuhamadtaufiq4@gmail.com>
2019-12-10 01:32:16 +0700

    fix allowed host

--- cutiapp/settings.py
+++ cutiapp/settings.py
@@ -62,9 +62,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_WHITELIST = (
-    'https://cutiapp.now.sh/',
-)
+CORS_ORIGIN_WHITELIST = ('https://cutiapp.now.sh',)
 
 CORS_ALLOW_CREDENTIALS = True
 
